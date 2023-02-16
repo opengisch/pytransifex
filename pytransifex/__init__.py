8b8046bc9fd092def492d126c6b1a2104f47f983
why-not-try-calmer <mrnycticorax@gmail.com>
2023-02-16 09:04:56 +0100

    Removed test for 3.10 from __init__

--- pytransifex/__init__.py
+++ pytransifex/__init__.py
@@ -1,9 +1,3 @@
 import logging
-from sys import version_info
-
-if version_info.major != 3 or version_info.minor < 10:
-    raise RuntimeError(
-        f"This program requires Python 3.10 at least, but found {version_info.major}.{version_info.minor}"
-    )
 
 logging.basicConfig(level=logging.INFO)
