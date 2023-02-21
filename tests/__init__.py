fdaa27270ed7f246588507ce1da8e94740c666b2
why-not-try-calmer <mrnycticorax@gmail.com>
2023-02-21 15:57:23 +0100

    Not catching exceptions when creating a project. Fixing typo swapping 'public' and 'private' in root __init__.py.

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,5 +13,5 @@ public = Path.cwd().joinpath("./tests/data/test_config_public.toml")
 test_config_public = toml.load(public)
 
 logging.info(
-    f"Running tests with this test_config: {test_config} and test_config_public: {test_config_public}"
+    f"Running tests with this test_config: {test_config_public} and test_config_public: {test_config_public}"
 )
